18ce78529b8b1e4bd50a1965f3c43951e780076e
JoseIgnacio <3046347@alu.murciaeduca.es>
2023-01-26 01:41:44 +0100

    Modal Windows with dependecies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,8 @@ module com.profesor.pelis {
     requires org.mongodb.bson;
     requires org.mongodb.driver.core;
     requires org.mongodb.driver.sync.client;
-
-    requires org.kordamp.ikonli.javafx;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires de.jensd.fx.glyphs.commons;
 
     opens com.profesor.pelis to javafx.fxml;
     exports com.profesor.pelis;
